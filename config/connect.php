e7cf4b0b738e26b9cdb9c5b67bfb7a912f3b1f3a
R.Gb <rdimasapit.bos@gmail.com>
2017-12-02 17:00:59 +0800

    db connection from mysql DaaS

--- config/connect.php
+++ config/connect.php
@@ -1,7 +1,7 @@
 <?php
-  $server = '192.168.3.66';
-  $username = 'hrmis';
-  $password = 'unknown192';
+  $server = 'db4free.net:3307';
+  $username = 'snjhshrm';
+  $password = 'nimdapatche031';
   $database = 'cio_spc';
   $conn = mysqli_connect($server, $username, $password, $database);
 ?>
